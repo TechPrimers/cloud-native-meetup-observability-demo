7239b924a8a9b685042ab73ffd04b0faba708de6
MovingToWeb <soleil.fils.88@gmail.com>
2020-03-03 11:37:09 +0530

    add comments

--- 1-start.sh
+++ 1-start.sh
@@ -1,8 +1,10 @@
 echo "Setting Up environment..."
-echo "gcloud config set project fleet-resolver-237016"
-gcloud config set project fleet-resolver-237016
+echo "gcloud config set project <PROJECT_ID>"
+#echo "gcloud config set project fleet-resolver-237016"
+gcloud config set project <PROJECT_ID>
+#gcloud config set project fleet-resolver-237016
 
-
+#cluster_name=standard-cluster-1
 echo "gcloud container clusters get-credentials standard-cluster-1 --zone us-central1-a"
 gcloud container clusters get-credentials standard-cluster-1 --zone us-central1-a
 
